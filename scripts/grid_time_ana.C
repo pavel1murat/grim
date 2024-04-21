a3edbe92c097c66e429c101839c39fd1e00579f7
Pavel Murat <murat@fnal.gov>
2024-04-21 17:16:49 -0500

    minor updates

--- scripts/grid_time_ana.C
+++ scripts/grid_time_ana.C
@@ -16,9 +16,15 @@ int grid_time_ana(const char* Project, const char* Stage, const char* Job, const
   //  t->ReadFile("/exp/mu2e/data/projects/pipenu/log/bmup4b0s36r0000.s4_digi_trig/000/timing_data/pipenu.bmup4b0s36r0000.s4_digi_trig.txt");
   t->ReadFile(fn.Data());
 
-  TH1F* h = new TH1F(Dsid,Dsid,1000,0,100);
+  TString hist_name;
+  if (Fileset == nullptr) hist_name = Form("%s.%s_%s",Dsid,Stage,Job);
+  else                    hist_name = Form("%s.%s.%s_%s",Dsid,Fileset,Stage,Job);
+  TH1F* h = new TH1F(hist_name.Data(),hist_name.Data(),1000,0,100);
+  h->GetXaxis()->SetTitle("segment wall time, hours");
   
-  t->Draw(Form("totwall/3600.>>%s.%s_%s",Dsid,Stage,Job));
+  t->Draw(Form("totwall/3600.>>%s",hist_name.Data()));
+
+  // c1->Print("pipenu/datasets/bmup4b0/timing_data/pipenu.bmup4b0s46r0000.000.s5_reco_kk.timing_data.png")
 
   return 0;
 }
